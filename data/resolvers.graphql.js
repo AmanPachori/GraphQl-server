9ec36d0fd9eb8172ce3f45d8c3024e2af8380498
AmanPachori <amangirrajpachori2002@gmail.com>
2023-03-30 22:27:19 +0530

    minor fixes

--- data/resolvers.graphql.js
+++ data/resolvers.graphql.js
@@ -1,42 +1,39 @@
-const mongoose = require('mongoose')
-const {modelSchema}  = require('../models/model');
-const userData = mongoose.model('userData',modelSchema);
-
+const mongoose = require("mongoose");
+const { modelSchema } = require("../models/model");
+const userData = mongoose.model("userData", modelSchema);
 
 const resolvers = {
   Query: {
-    getAlluser:(root)=>{
-      return new Promise((resolve,reject)=>{
-          userData.find((err,users)=>{
-              if(err) reject(err);
-              else resolve(users);
-          })
-      })
+    getAlluser: (root) => {
+      return new Promise((resolve, reject) => {
+        userData.find((err, users) => {
+          if (err) reject(err);
+          else resolve(users);
+        });
+      });
+    },
   },
-},
-Mutation :{
-  createuser: (root,{ input }) => {
-    const newuser=new userData({
-        price : input.price,
-        address : input.address,
-        image : input.image,
-        noOfBedroom : input.noOfBedroom,
-        noOfBathroom : input.noOfBathroom,
-        area: input.area
-    });
+  Mutation: {
+    createuser: (root, { input }) => {
+      const newuser = new userData({
+        price: input.price,
+        address: input.address,
+        image: input.image,
+        noOfBedroom: input.noOfBedroom,
+        noOfBathroom: input.noOfBathroom,
+        area: input.area,
+      });
 
-    newuser.id=newuser._id;
+      newuser.id = newuser._id;
 
-    return new Promise((resolve,reject)=>{
-        newuser.save((err)=>{
-            if(err) reject(err);
-            else resolve(newuser);
-        })
-    })
-
-}
-}
-
+      return new Promise((resolve, reject) => {
+        newuser.save((err) => {
+          if (err) reject(err);
+          else resolve(newuser);
+        });
+      });
+    },
+  },
 };
 
-module.exports ={resolvers};
+module.exports = { resolvers };
